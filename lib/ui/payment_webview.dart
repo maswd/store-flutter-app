92ce87d703faf76539c8c1e9eeebaa58dc3442d1
Masoud Gorgani <alone.3303@gmail.com>
2024-07-20 17:40:26 +0330

    receipt payment dargah par dakht

--- lib/ui/payment_webview.dart
+++ lib/ui/payment_webview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:store/ui/receipt/payment_receipt.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class PaymentGatewayScreen extends StatelessWidget {
@@ -11,6 +12,20 @@ class PaymentGatewayScreen extends StatelessWidget {
     return WebView(
       initialUrl: bankGatewayUrl,
       javascriptMode: JavascriptMode.unrestricted,
+      onPageStarted: (url) {
+        debugPrint("url : $url");
+        final uri = Uri.parse(url);
+        debugPrint("host : $uri.host");
+
+        if (uri.pathSegments.contains("appCheckout") &&
+            uri.host =="expertdevelopers.ir") {
+          final orderId = int.parse(uri.queryParameters["order_id"]!);
+          Navigator.of(context).pop();
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => PaymentReceiptScreen(orderId: orderId),
+          ));
+        }
+      },
     );
   }
 }
